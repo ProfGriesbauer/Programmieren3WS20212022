30c4897ed3034a069ce8d4d1a5ca27f26bcdb476
Michael Holzmann <michaelholzmann7@gmail.com>
2021-11-15 10:00:38 +0100

    Move eingefügt

--- OOPGames/OOPGames/Classes/TicTacToe/GC_TicTacToe.cs
+++ OOPGames/OOPGames/Classes/TicTacToe/GC_TicTacToe.cs
@@ -16,4 +16,24 @@ using System.Windows.Shapes;
 
 namespace OOPGames
 {
+
+    public class GC_TicTacToeMove : ITicTacToeMove
+    {
+        int _Row = 0;
+        int _Column = 0;
+        int _PlayerNumber = 0;
+
+        public GC_TicTacToeMove(int row, int column, int playerNumber)
+        {
+            _Row = row;
+            _Column = column;
+            _PlayerNumber = playerNumber;
+        }
+
+        public int Row { get { return _Row; } }
+
+        public int Column { get { return _Column; } }
+
+        public int PlayerNumber { get { return _PlayerNumber; } }
+    }
 }
