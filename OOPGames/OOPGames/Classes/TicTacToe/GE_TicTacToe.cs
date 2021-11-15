c612f9a3d1f7297cbae83508e80a04b93ce8dd11
TimRuss01 <91973320+TimRuss01@users.noreply.github.com>
2021-11-15 08:37:09 +0100

    Dialogfenster Beispiele

--- OOPGames/OOPGames/Classes/TicTacToe/GE_TicTacToe.cs
+++ OOPGames/OOPGames/Classes/TicTacToe/GE_TicTacToe.cs
@@ -355,3 +355,85 @@ namespace OOPGames
         }
     }*/
 }
+
+/*
+ Beispiele DialogWindow:
+
+
+    using System;
+using System.Windows;
+
+namespace WpfTutorialSamples.Dialogs
+{
+	public partial class InputDialogSample : Window
+	{
+		public InputDialogSample(string question, string defaultAnswer = "")
+		{
+			InitializeComponent();
+			lblQuestion.Content = question;
+			txtAnswer.Text = defaultAnswer;
+		}
+
+		private void btnDialogOk_Click(object sender, RoutedEventArgs e)
+		{
+			this.DialogResult = true;
+		}
+
+		private void Window_ContentRendered(object sender, EventArgs e)
+		{
+			txtAnswer.SelectAll();
+			txtAnswer.Focus();
+		}
+
+		public string Answer
+		{
+			get { return txtAnswer.Text; }
+		}
+	}
+}
+
+using System.Windows.Forms;
+using System.Drawing;
+
+public static DialogResult InputBox(string title, string promptText, ref string value)
+{
+  Form form = new Form();
+  Label label = new Label();
+  TextBox textBox = new TextBox();
+  Button buttonOk = new Button();
+  Button buttonCancel = new Button();
+
+  form.Text = title;
+  label.Text = promptText;
+  textBox.Text = value;
+
+  buttonOk.Text = "OK";
+  buttonCancel.Text = "Cancel";
+  buttonOk.DialogResult = DialogResult.OK;
+  buttonCancel.DialogResult = DialogResult.Cancel;
+
+  label.SetBounds(9, 20, 372, 13);
+  textBox.SetBounds(12, 36, 372, 20);
+  buttonOk.SetBounds(228, 72, 75, 23);
+  buttonCancel.SetBounds(309, 72, 75, 23);
+
+  label.AutoSize = true;
+  textBox.Anchor = textBox.Anchor | AnchorStyles.Right;
+  buttonOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+  buttonCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+
+  form.ClientSize = new Size(396, 107);
+  form.Controls.AddRange(new Control[] { label, textBox, buttonOk, buttonCancel });
+  form.ClientSize = new Size(Math.Max(300, label.Right + 10), form.ClientSize.Height);
+  form.FormBorderStyle = FormBorderStyle.FixedDialog;
+  form.StartPosition = FormStartPosition.CenterScreen;
+  form.MinimizeBox = false;
+  form.MaximizeBox = false;
+  form.AcceptButton = buttonOk;
+  form.CancelButton = buttonCancel;
+
+  DialogResult dialogResult = form.ShowDialog();
+  value = textBox.Text;
+  return dialogResult;
+}
+*/
