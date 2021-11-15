b3ced9a61fba5d1636b9303fc481028020645abb
Berger <markus.berger@stud.hs-kempten.de>
2021-11-15 10:41:12 +0100

    Spieler Gruppe C

--- OOPGames/OOPGames/Classes/TicTacToe/GC_TicTacToe.cs
+++ OOPGames/OOPGames/Classes/TicTacToe/GC_TicTacToe.cs
@@ -61,4 +61,39 @@ namespace OOPGames
         }
 
     }
+    public class SpielerGruppeC : BaseHumanTicTacToePlayer
+    {
+        int _PlayerNumber = 0;
+
+        public override string Name { get { return "Spieler Gruppe C"; } }
+
+        public override IGamePlayer Clone()
+        {
+            SpielerGruppeC ttthp = new SpielerGruppeC();
+            ttthp.SetPlayerNumber(_PlayerNumber);
+            return ttthp;
+        }
+
+        public override ITicTacToeMove GetMove(IMoveSelection selection, ITicTacToeField field)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (selection.XClickPos > 20 + (j * 100) && selection.XClickPos < 120 + (j * 100) &&
+                        selection.YClickPos > 20 + (i * 100) && selection.YClickPos < 120 + (i * 100))
+                    {
+                        return new TicTacToeMove(i, j, _PlayerNumber);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public override void SetPlayerNumber(int playerNumber)
+        {
+            _PlayerNumber = playerNumber;
+        }
+    }
 }
