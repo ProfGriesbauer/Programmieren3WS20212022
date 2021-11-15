f6e43604b6dc247ca9a14b0d8e064bddcf834c0f
julian-kofler <julian.kofler@stud.hs-kempten.de>
2021-11-15 10:32:43 +0100

    human player hinzugefügt

--- OOPGames/OOPGames/Classes/TicTacToe/Gh_TicTacToe.cs
+++ OOPGames/OOPGames/Classes/TicTacToe/Gh_TicTacToe.cs
@@ -361,4 +361,41 @@ namespace OOPGames
             }
         }
     }
+
+    public class Gh_TicTacToeHumanPlayer : BaseHumanTicTacToePlayer
+    {
+        int _PlayerNumber = 0;
+
+        public override string Name { get { return "GriesbauerHumanTicTacToePlayer"; } }
+
+        public override IGamePlayer Clone()
+        {
+            TicTacToeHumanPlayer ttthp = new TicTacToeHumanPlayer();
+            ttthp.SetPlayerNumber(_PlayerNumber);
+            return ttthp;
+        }
+
+        public override ITicTacToeMove GetMove(IMoveSelection selection, ITicTacToeField field)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (selection.XClickPos > 20 + (j * 100) && selection.XClickPos < 120 + (j * 100) &&
+                        selection.YClickPos > 20 + (i * 100) && selection.YClickPos < 120 + (i * 100))
+                    {
+                        return new TicTacToeMove(i, j, _PlayerNumber);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public override void SetPlayerNumber(int playerNumber)
+        {
+            _PlayerNumber = playerNumber;
+        }
+    }
+
 }
